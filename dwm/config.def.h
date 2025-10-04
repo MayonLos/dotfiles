d311b91cc17534cb35eda56deac98c7c60db48c6
MayonLos <ml20061023@outlook.com>
2025-10-04 13:56:52 +0800

    dwl-beautify

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,14 +7,14 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Blod:size=14" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Blod:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
+static const char col_gray1[] = "#1e2127";
+static const char col_gray2[] = "#282c34";
+static const char col_gray3[] = "#abb2bf";
+static const char col_gray4[] = "#d7dae0";
+static const char col_cyan[]  = "#5c6370";
+static const unsigned int baralpha = 0xcc;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
